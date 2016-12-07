1290d9cb2f395612a2f88b7e7d861d08013bb5f1
RadhaGit <radhakota27@gmail.com>
2016-12-07 09:38:21 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
-public class Sample{
-public static void main(String[args]){
-System.out.println("Hello World");
-}
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
 }
